7a83b1918401ee372b7da1f36058f98b10f988db
niko <niko@localhost>
2025-02-13 21:39:43 -0500

    funciones básicas completadas

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -8,7 +8,7 @@ import co.edu.unbosque.view.Window;
 public class Controller implements ActionListener{
 	
 	private Window window;
-	private float g = 9.8f;
+	private float g = 9.81f;
 	public Controller() {
 		
 		window = new Window();
@@ -29,20 +29,44 @@ public class Controller implements ActionListener{
 				double theta = Double.parseDouble(window.getMainPanel().getAngle().getText());
 				System.out.println(calcMaxHeight(vo, theta));
 			}
+			if (window.getMainPanel().getX_max().getText().equals("")) {
+				float vo = Float.parseFloat( window.getMainPanel().getInitial_speed().getText());
+				double theta = Double.parseDouble(window.getMainPanel().getAngle().getText());
+				System.out.println(calcMaxReach(vo, theta));
+			}
+			if (window.getMainPanel().getFly_time().getText().equals("")) {
+				float vo = Float.parseFloat( window.getMainPanel().getInitial_speed().getText());
+				double theta = Double.parseDouble(window.getMainPanel().getAngle().getText());
+				System.out.println(flightTIme(vo, theta));
+			}
 		}
 		
 	}
 	
-	public float calcMaxHeight(float initial_velocity, double angle ) {
+	public float calcMaxHeight(float initial_velocity, double angle) {
 		
-		
-		float numerator = (float) (Math.pow(initial_velocity, 2)*Math.pow((Math.sin(angle)),2));
+		angle = Math.toRadians(angle);
+		float numerator = (float) ((initial_velocity)*(initial_velocity)*Math.sin(angle)*Math.sin(angle));
 		float denominator = g*2;
 		float y_max= numerator/denominator;
 		
 		return y_max;
 	}
 	
+	
+	public float calcMaxReach(float initial_velocity, double angle) {
+		angle = 2*Math.toRadians(angle);
+		float numerator = (float) (initial_velocity*initial_velocity*Math.sin(angle));
+		float x_max = numerator/g;
+		return x_max;
+	}
+	
+	public float flightTIme(float initial_velocity, double angle) {
+		angle = Math.toRadians(angle);
+		float numerator = (float) (2*initial_velocity*Math.sin(angle));
+		float x_max = numerator/g;
+		return x_max;
+	}
 	
 }
 
